9928ac51471554b568b54db33a1feaff95862b09
Merik <merik.chen@gmail.com>
2016-03-14 02:55:10 +0800

    Upd SPIDER config address

--- python/Config.py
+++ python/Config.py
@@ -7,12 +7,12 @@ import re
 
 app_env = 'app_env' in os.environ and str(os.environ['app_env']).lower() or ''
 
-if app_env == 'dev':
+if app_env == 'SPIDER':
     app_cfg = {
-        'mongo': {'address': '192.168.10.254', 'port': 27017},
-        'redis': {'address': '192.168.10.254', 'port': 6384},
-        'gearman': {'address': '192.168.10.254', 'port': 4730},
-        'memcached': {'address': '192.168.10.254', 'port': 11211},
+        'mongo': {'address': '192.168.122.1', 'port': 27017},
+        'redis': {'address': '192.168.122.1', 'port': 6384},
+        'gearman': {'address': '192.168.122.1', 'port': 4730},
+        'memcached': {'address': '192.168.122.1', 'port': 11211},
     }
 elif app_env == 'master':
     app_cfg = {
