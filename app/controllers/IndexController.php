66b584b6d778354d7aa57c3312f8774aebfcc611
Merik <merik.chen@gmail.com>
2016-03-19 01:32:04 +0800

    Upd index css

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,7 +5,7 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-        $this->view->latest = $this->article->getLatestArticle();
+        $this->view->latest = $this->article->getLatestArticle(50);
     }
 
     public function detailAction()
