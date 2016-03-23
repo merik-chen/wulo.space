1e66def411e2a6fad4dfac1ee86277bf8e1e7779
Merik <merik.chen@gmail.com>
2016-03-23 22:35:23 +0800

    Upd

--- python/Dcard/scrapyLists.py
+++ python/Dcard/scrapyLists.py
@@ -8,6 +8,7 @@ import requests
 import random
 import Cookie
 import time
+import json
 
 INITIAL_ENDPOINT = 'https://www.dcard.tw/f'
 INITIAL_BOARD_LIST_ENDPOINT = 'https://www.dcard.tw/api/forum'
@@ -15,7 +16,7 @@ USER_AGENT = random_ua()
 COOKIE = Cookie.SimpleCookie()
 COOKIE_XSRF = ''
 BOARDS = {}
-NOW_BOARD = {}
+NOW_BOARD = ''
 
 LIST_ENDPOINT_PREFIX = 'https://www.dcard.tw/api/forum/%s/%s/'
 LIST_ENDPOINT_HEADER = {
@@ -58,7 +59,13 @@ def get_board_list():
 
     if r.status_code == 200:
         parse_xsrf_token(r.headers['set-cookie'])
-        return r.json()
+        boards = r.json()['forum']
+        for board in boards:
+            JobClient.submit_job(
+                'dcard-scarp-board',
+                board['alias'].encode('utf-8'),
+                background=True
+            )
     else:
         print 'Initial connection failed.'
         exit()
@@ -67,7 +74,7 @@ def get_board_list():
 def scrap_list(page):
     global COOKIE, USER_AGENT, COOKIE_XSRF, LIST_ENDPOINT_HEADER, LIST_ENDPOINT_PREFIX, NOW_BOARD
     r = requests.get(
-        LIST_ENDPOINT_PREFIX % (NOW_BOARD['alias'].encode('utf-8'), page.encode('utf-8')),
+        LIST_ENDPOINT_PREFIX % (NOW_BOARD.encode('utf-8'), page.encode('utf-8')),
         headers=LIST_ENDPOINT_HEADER
     )
 
@@ -75,42 +82,103 @@ def scrap_list(page):
         parse_xsrf_token(r.headers['set-cookie'])
         return r.json()
 
-if '__main__' == __name__:
+
+def dcard_scarp_board(gearman_worker, gearman_job):
+    global USER_AGENT, INITIAL_PAGE, NOW_BOARD
+    NOW_BOARD = gearman_job.data
     initial_connect()
-    BOARDS = get_board_list()['forum']
-    NOW_BOARD = BOARDS.pop(0)
-    isContinue = True
-    while isContinue:
+    is_continue = True
+    INITIAL_PAGE = 1
+    while is_continue:
         try:
-            isContinue = False
+            is_continue = False
             random_sleep = random.randrange(5, 30)
             USER_AGENT = random_ua()
             for index, data in enumerate(scrap_list(str(INITIAL_PAGE))):
                 if RawDatabase.find_one({'id': data['id']}) is None:
-                    isContinue = True or isContinue
+                    is_continue = True or is_continue
                     RawDatabase.save(data)
             print 'Dcard: Scraped %s page %s.\tSleep %s sec(s).' % (
-                NOW_BOARD['alias'].encode('utf-8'),
+                NOW_BOARD.encode('utf-8'),
                 INITIAL_PAGE,
                 random_sleep
             )
             time.sleep(random_sleep)
 
-            if isContinue:
+            if is_continue:
                 INITIAL_PAGE += 1
             else:
-                if len(BOARDS) > 0:
-                    print "This Board: %s scraped. Do next.\n" % NOW_BOARD['alias'].encode('utf-8')
-                    NOW_BOARD = BOARDS.pop(0)
-                    INITIAL_PAGE = 1
-                    isContinue = True
+                print "This Board: %s scraped. Do next.\n" % NOW_BOARD.encode('utf-8')
+                print "Checking board remains in pool...\t"
+                _board_remain = SimpleGearManAdmin.SimpleGearManAdmin(
+                    app_cfg['gearman']['address'],
+                    app_cfg['gearman']['port']
+                ).get_status('dcard-scarp-board')
+                if (_board_remain is None) or (_board_remain['queued'] == 0):
+                    get_board_list()
+                    print "Re-Filling.\n"
+                else:
+                    print "Enough.\n"
 
-        except KeyboardInterrupt:
-            print "\nBye"
-            sys.exit()
         except 'Exception' as e:
             traceback.print_exc()
             sys.exit()
 
-    print "No more update, Bye~\n"
-    sys.exit()
+    print 'Sleep for 5 minutes.\n'
+    time.sleep(5 * 60)
+
+    return 'ok'
+
+if '__main__' == __name__:
+    print app_env
+
+    board_remain = SimpleGearManAdmin.SimpleGearManAdmin(
+        app_cfg['gearman']['address'],
+        app_cfg['gearman']['port']
+    ).get_status('dcard-scarp-board')
+
+    if (board_remain is None) or (board_remain['queued'] == 0):
+        get_board_list()
+
+    JobWorker.register_task('dcard-scarp-board', dcard_scarp_board)
+    JobWorker.work()
+
+    # initial_connect()
+    # BOARDS = get_board_list()['forum']
+    # NOW_BOARD = BOARDS.pop(0)
+    #
+    # isContinue = True
+    # while isContinue:
+    #     try:
+    #         isContinue = False
+    #         random_sleep = random.randrange(5, 30)
+    #         USER_AGENT = random_ua()
+    #         for index, data in enumerate(scrap_list(str(INITIAL_PAGE))):
+    #             if RawDatabase.find_one({'id': data['id']}) is None:
+    #                 isContinue = True or isContinue
+    #                 RawDatabase.save(data)
+    #         print 'Dcard: Scraped %s page %s.\tSleep %s sec(s).' % (
+    #             NOW_BOARD['alias'].encode('utf-8'),
+    #             INITIAL_PAGE,
+    #             random_sleep
+    #         )
+    #         time.sleep(random_sleep)
+    #
+    #         if isContinue:
+    #             INITIAL_PAGE += 1
+    #         else:
+    #             if len(BOARDS) > 0:
+    #                 print "This Board: %s scraped. Do next.\n" % NOW_BOARD['alias'].encode('utf-8')
+    #                 NOW_BOARD = BOARDS.pop(0)
+    #                 INITIAL_PAGE = 1
+    #                 isContinue = True
+    #
+    #     except KeyboardInterrupt:
+    #         print "\nBye"
+    #         sys.exit()
+    #     except 'Exception' as e:
+    #         traceback.print_exc()
+    #         sys.exit()
+    #
+    # print "No more update, Bye~\n"
+    # sys.exit()
