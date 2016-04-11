0a7fcadf4501bcbfe14d2f0270a312a7af0d4d81
Merik <merik.chen@gmail.com>
2016-04-11 23:34:36 +0800

    Upd

--- python/meregeAccount.py
+++ python/meregeAccount.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from scrapy import Selector
 from Exceptions.InputError import InputError
 import traceback
 import Database
@@ -13,15 +14,21 @@ AccountDB = Database.Mongo['account']['id_table']
 DcardDB = Database.Mongo['Dcard']['raw_posts']
 PttDB = Database.Mongo['wulo']['data']
 
-for post in PttDB.find():
-    if 'author' in post:
-        nick = 'nick' in post and post['nick'] or post['author']
-        AccountDB.find_one_and_update(
-            {'id': post['author']},
-            {
-                '$set': {
-                    'id': post['author'],
-                    'associate.ptt': post['nick']
-                }
-            }, upsert=True
-        )
+for post in Database.Mongo['wulo']['raw']:
+    selector = Selector(text=post['html'])
+    for user in selector.css('.push-userid::text'):
+        print (user)
+    exit()
+
+# for post in PttDB.find():
+#     if 'author' in post:
+#         nick = 'nick' in post and post['nick'] or post['author']
+#         AccountDB.find_one_and_update(
+#             {'id': post['author']},
+#             {
+#                 '$set': {
+#                     'id': post['author'],
+#                     'associate.ptt': post['nick']
+#                 }
+#             }, upsert=True
+#         )
