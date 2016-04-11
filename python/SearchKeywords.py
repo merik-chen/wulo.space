38b1884635f834e78debd368d35dbc5607b4ef66
Merik <merik.chen@gmail.com>
2016-04-11 22:20:58 +0800

    Upd

--- python/SearchKeywords.py
+++ python/SearchKeywords.py
@@ -46,11 +46,9 @@ def fetch_mobile_index():
     url = u'https://tw.mobi.yahoo.com/_td/api/resource/TrendingService;treningNowUrl=http%3A%2F%2Ftw.kvc.search.yahoo.com%3A4080%2Fkv%2Fget%3Fformat%3Djson%26type%3Dmetro%26src%3Dmetro%26order%3Drank%26count%3D8%26ns%3Dzh_tw%26appid%3Dtn_uh_tw%26key%3Dtn_general_uni;update=true?bkt=&device=smartphone&intl=tw&lang=zh-Hant-TW&partner=none&region=TW&site=fp&tz=Asia%2FTaipei&ver=1.1.161&returnMeta=true'
     r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.10+ (KHTML, like Gecko) Version/10.0.9.2372 Mobile Safari/537.10+'})
     if r.status_code == 200:
-        response = json.loads(json.loads(r.text.encode('utf-8', 'ignore')))
-        for index, keyword in response.items():
-            if index != u'title':
-                temp = dict(urlparse.parse_qsl(urlparse.urlparse(keyword[u'link']).query.encode('utf-8')))
-                rst.append(temp[u'p'])
+        response = json.loads(r.json()['data'])
+        for keyword in response['itemsInfo']['items']:
+            rst.append(keyword[u'search_term'])
     return rst
 
 
@@ -68,7 +66,7 @@ def fetch():
 
     for keyword in keywords:
         if not Database.Redis.zscore(RedisKey, keyword):
-            Database.Redis.zadd(RedisKey, keyword, 1)
+            Database.Redis.zadd(RedisKey, keyword, time.time())
         if not Database.Redis.zscore(RedisKeyOT, keyword):
             Database.Redis.zadd(RedisKeyOT, keyword, 1)
 
@@ -86,7 +84,7 @@ def fetch():
         response = json.loads(r.text.encode('utf-8', 'ignore'))
         for kw in response['trendsByDateList'][0]['trendsList']:
             if not Database.Redis.zscore(RedisKey, kw['title']):
-                Database.Redis.zadd(RedisKey, kw['title'], 1)
+                Database.Redis.zadd(RedisKey, kw['title'], time.time())
             if not Database.Redis.zscore(RedisKeyOT, kw['title']):
                 Database.Redis.zadd(RedisKeyOT, kw['title'], 1)
 
