7081315dba5fc9c61ffbd65fad89d17757f27bbe
Merik <merik@Meriks-MacBook-Pro.local>
2016-04-21 11:18:40 +0800

    Upd

--- python/Config.py
+++ python/Config.py
@@ -9,8 +9,8 @@ app_env = 'APP_ENV' in os.environ and str(os.environ['APP_ENV']).lower() or ''
 
 if app_env == 'spider':
     app_cfg = {
-        'mongo': {'address': '192.168.122.1', 'port': 27017},
-        'redis': {'address': '192.168.122.1', 'port': 6379},
+        'mongo': {'address': '192.168.122.253', 'port': 27017},
+        'redis': {'address': '192.168.122.253', 'port': 6379},
         'gearman': {'address': '192.168.122.1', 'port': 4730},
         'memcached': {'address': '192.168.122.1', 'port': 11211},
     }
