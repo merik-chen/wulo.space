fb1058920b2361dae5b75f8c67943255d4bec701
Merik <merik@Meriks-MacBook-Pro.local>
2016-04-27 17:11:58 +0800

    Upd

--- app/models/Base.php
+++ app/models/Base.php
@@ -47,7 +47,7 @@ class Base extends Phalcon\Mvc\Model
 
     protected function initMongo()
     {
-        $mongo = new \MongoClient("mongodb://192.168.122.253:27017");
+        $mongo = new \MongoClient("mongodb://192.168.122.253:27017", ['replicaSet' => 'dbrepl']);
         return $mongo;
     }
 
