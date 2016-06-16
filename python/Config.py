a4ef268175514f2fcfcaac4c1ac7d06469a11fcb
Merik-VT <merik@voicetube.com>
2016-06-16 23:51:47 +0800

    Upd

--- python/Config.py
+++ python/Config.py
@@ -10,7 +10,7 @@ app_env = 'APP_ENV' in os.environ and str(os.environ['APP_ENV']).lower() or ''
 if app_env == 'spider':
     app_cfg = {
         'mongo': {'address': '192.168.10.251', 'port': 27017},
-        'redis': {'address': '192.168.122.254', 'port': 6379},
+        'redis': {'address': '192.168.10.251', 'port': 6379},
         'gearman': {'address': '192.168.122.1', 'port': 4730},
         'memcached': {'address': '192.168.122.1', 'port': 11211},
         'mongo_replica': ['192.168.10.251:27017']
@@ -18,7 +18,7 @@ if app_env == 'spider':
 elif app_env == 'master':
     app_cfg = {
         'mongo': {'address': '192.168.10.251', 'port': 27017},
-        'redis': {'address': '192.168.10.40', 'port': 6379},
+        'redis': {'address': '192.168.10.251', 'port': 6379},
         'gearman': {'address': '192.168.10.254', 'port': 4730},
         'memcached': {'address': '192.168.10.254', 'port': 11211},
         'mongo_replica': ['192.168.10.251:27017']
@@ -26,9 +26,9 @@ elif app_env == 'master':
 else:
     app_cfg = {
         'mongo': {'address': '192.168.10.251', 'port': 27017},
-        'redis': {'address': '192.168.10.40', 'port': 6379},
+        'redis': {'address': '192.168.10.251', 'port': 6379},
         'gearman': {'address': '192.168.10.254', 'port': 4730},
-        'memcached': {'address': '192.168.10.254', 'port': 11211},
+        'memcached': {'address': '127.0.0.1', 'port': 11211},
         'mongo_replica': ['192.168.10.251:27017']
     }
 
