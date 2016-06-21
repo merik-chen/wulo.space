bcd1c91f3b8a9ad87281339c1af06122fb3cb8fe
Merik-VT <merik@voicetube.com>
2016-06-22 00:47:12 +0800

    upd

--- python/DcardV2/Article.py
+++ python/DcardV2/Article.py
@@ -43,6 +43,7 @@ class Article:
             self.COOKIE_XSRF = self.utility.parse_xsrf_token(r.headers)
             return r.json()
         else:
+            print(r.status_code, r.content)
             raise InputError('Can not get the article.', None)
 
 
